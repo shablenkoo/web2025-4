be3ddf50b95f9a52e863562731fae28791eb2896
Alina web2025-4 <shablenkoo@gmail.com>
2025-03-18 17:07:23 +0200

    Додала використання Commander.js для роботи з аргументами командного рядка

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+const { program } = require('commander');
+
+program.version('1.0.0');
+
+program
+  .option('-n, --name <name>', 'Введіть ваше імʼя')
+  .parse(process.argv); 
+
+const options = program.opts();
+
+if (options.name) {
+  console.log(`Привіт, ${options.name}!`);
+} else {
+  console.log('Привіт, світе!');
+}
+
