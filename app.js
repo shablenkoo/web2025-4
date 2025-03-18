42abd1323af5ef2596be43476b6c696d91df086f
Alina web2025-4 <shablenkoo@gmail.com>
2025-03-18 17:28:27 +0200

    Додала підтримку параметрів командного рядка для запуску серверу

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+const fs = require('fs');
 const { program } = require('commander');
-
-program.version('1.0.0');
+const http = require('http');
 
 program
-  .option('-n, --name <name>', 'Введіть ваше імʼя')
-  .parse(process.argv); 
+  .requiredOption('-h, --host <host>', 'Адреса сервера')
+  .requiredOption('-p, --port <port>', 'Порт сервера')
+  .requiredOption('-i, --input <input>', 'Шлях до файлу JSON')
+  .parse(process.argv);
 
 const options = program.opts();
 
-if (options.name) {
-  console.log(`Привіт, ${options.name}!`);
-} else {
-  console.log('Привіт, світе!');
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);  // Виходимо з помилкою
 }
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Сервер запущено на ${options.host}:${options.port}`);
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Сервер запущено на http://${options.host}:${options.port}`);
+});
 
